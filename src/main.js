40962ab16f206f704077beabd99618cebd43da54
xulizhe <xulizhe@superatomfin.com>
2019-08-29 19:02:52 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible'
+import './assets/scss/index.scss'
 Vue.config.productionTip = false
 
 new Vue({
